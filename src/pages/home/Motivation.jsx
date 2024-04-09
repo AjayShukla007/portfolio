e2eea8fb6fa6fed364be513ffd2815132400790f
AjayShukla007 <shuklaajay0838@gmail.com>
2024-04-09 11:10:47 +0530

    Removing unused code

--- src/pages/home/Motivation.jsx
+++ src/pages/home/Motivation.jsx
@@ -2,19 +2,19 @@
 function Motivation() {
   return (
     <div className='lineDiv'>
-   <div className="line line1">
-     
-   </div>
-   <div className="line line2">
-     
-   </div>
-   <div className="queto">
-    this ajay
-   </div>
-   <div className="line line3">
-     
-   </div>
-  </div>
+      <div className="line line1">
+
+      </div>
+      <div className="line line2">
+
+      </div>
+      <div className="queto">
+        this ajay
+      </div>
+      <div className="line line3">
+
+      </div>
+    </div>
   )
 }
 
--- src/pages/home/Name.jsx
+++ src/pages/home/Name.jsx
@@ -1,7 +1,6 @@
-// import { useEffect, useCallback, useRef } from "react";
 
 function Name() {
   return <h1 className="Name">Ajay Shukla</h1>;
 }
 
-export {Name};
+export { Name };
