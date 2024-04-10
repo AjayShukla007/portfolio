c1b85466a9d452069a969e1bc7b103950a4d6c83
AjayShukla007 <shuklaajay0838@gmail.com>
2024-04-10 12:12:33 +0530

    Improved code

--- src/pages/project/Projects.jsx
+++ src/pages/project/Projects.jsx
@@ -1,22 +1,15 @@
 import React, { useState, useEffect } from "react";
 import {
   motion,
-  useAnimation,
-  useInView,
-  motionValue,
   useTransform,
-  useVelocity,
-  useSpring,
-  useScroll,
-  useMotionValueEvent,
-  useMotionValue
+  useScroll
 } from "framer-motion";
 import { useQuery } from "react-query";
 import axios from "axios";
 
 import "./styles/projects.css";
 import ProjectItems from "./ProjectItems.jsx";
-import projectData from "./ProjectData.jsx";
+// import projectData from "./ProjectData.jsx";
 import useElementScrollVelocity from "../../hook/ScrollVelocity.jsx";
 import ProjectsPreload from '../../components/PreLoaders/ProjectsPreload.jsx';
 const getProjectUrl = import.meta.env.VITE_API_GET_Projects;
@@ -47,36 +40,10 @@ function Projects() {
   const { scrollY } = useScroll({
     target: mainRef
   });
-  const y1 = useTransform(scrollY, [0, 300], [0, 200]);
   const y2 = useTransform(scrollY, [0, 300], [0, -100]);
 
-  /* const [targetElement, setElement] = useState();
-  useEffect(() => {
-    setElement(mainRef.current);
-  }, []);
-  */
-
-  const baseX = useMotionValue(0);
-  const scrollVelocity = useVelocity(scrollY);
-  const smoothVelocity = useSpring(scrollVelocity, {
-    damping: 50,
-    stiffness: 400
-  });
-
-  const y = useMotionValue(0);
-
-  const ySmooth = useSpring(y, { damping: 50, stiffness: 400 });
-  // const yVelocity = useVelocity(scrollY);
   const yVelocity = useElementScrollVelocity(mainRef);
-  const scale = useTransform(yVelocity, [-3000, 0, 3000], [2, 1, 2], {
-    clamp: false
-  });
 
-  const backgroundColor = useTransform(
-    yVelocity,
-    [-2000, 0, 2000],
-    [pink(100), pink(0), pink(100)]
-  );
   let filter = useTransform(
     yVelocity,
     [-2000, 0, 2000],
@@ -89,7 +56,7 @@ function Projects() {
       ref={mainRef}
       style={{ filter, transition: "1s linear all" }}
     >
-      {data?data.map(data => {
+      {data ? data.map(data => {
         const langKey = data.tags.split(' ').map(tags => {
           const tag = tags.split("");
           return (
@@ -111,10 +78,10 @@ function Projects() {
             y1={y2}
           />
         );
-      }):(<>
-      <ProjectsPreload/>
-      <ProjectsPreload/>
-      <ProjectsPreload/>
+      }) : (<>
+        <ProjectsPreload />
+        <ProjectsPreload />
+        <ProjectsPreload />
       </>)}
     </motion.div>
   );
