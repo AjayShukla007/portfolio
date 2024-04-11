04bc3228f4a4f20b731521ac764bd67d12f7a618
AjayShukla007 <shuklaajay0838@gmail.com>
2024-04-11 19:13:14 +0530

    Improved code

--- src/pages/caseStudies/CaseStudies.jsx
+++ src/pages/caseStudies/CaseStudies.jsx
@@ -3,38 +3,8 @@ import CsItems from "./CsItems.jsx";
 import "./style/csStyles.css";
 // import { useTitleData } from "../../components/context/csContext.jsx";
 
-/* const fetchedData = [
-  {
-    id: 1,
-    title: "title",
-    flow: "flowChart",
-    description: "description",
-    techUsed: "tech used",
-    motivation: "motivation to build this project"
-  },
-  {
-    id: 2,
-    title: "title",
-    flow: "flowChart",
-    description: "description",
-    techUsed: "tech used",
-    motivation: "motivation to build this project"
-  },
-  {
-    id: 3,
-    title: "title",
-    flow: "flowChart",
-    description: "description",
-    techUsed: "tech used",
-    motivation: "motivation to build this project"
-  }
-]; */
-
-
 function CaseStudies() {
   
-// const {title} = useTitleData();
-
   return (
     <div className="CaseStudies">
       <CsNav/>
