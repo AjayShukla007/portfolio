04a9979adf65b04e36131e16c8ce8b2a886a4945
AjayShukla007 <shuklaajay0838@gmail.com>
2024-04-30 19:29:31 +0530

    Adding error page component

---
+++ src/pages/utility/Error.jsx
@@ -0,0 +1,14 @@
+import SpacielPage from '../../components/extraPages/ExtraPage'
+//add line breake in info
+const Error = () => {
+  return (
+    <div>
+        <SpacielPage 
+        header="Error"
+        code='Some Unknown Error Occured'
+        info={"This may not mean anything. \n \nI'm probably working on something that has blown up"}/>
+    </div>
+  )
+}
+
+export default Error
