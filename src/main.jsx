d557e85d8fe346da16645719a9cedbb3d4ed430e
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-01 14:40:29 +0530

    Working on error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter as Router} from "react-router-dom";
-
+import { BrowserRouter as Router } from "react-router-dom";
+import ErrorBoundary from './components/Interactive/ErrorBoundary.jsx';
 import './index.css'
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </Router>
   </React.StrictMode>
 )
