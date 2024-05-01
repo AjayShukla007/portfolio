18645c6d10206f3a31c5ba2f8360c5cb0055c2e1
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-01 16:54:57 +0530

    Working on special pages

--- src/components/extraPages/ExtraPage.jsx
+++ src/components/extraPages/ExtraPage.jsx
@@ -2,16 +2,17 @@ import PropTypes from 'prop-types';
 
 const SpacielPage = (props) => {
   return (
-    <div>
-        <div className="header">{props.header}</div>
-        <div className="code">{props.code}</div>
-        <div className="info">{props.info}</div>
-    </div>
+    <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg">
+        <h1>:)</h1>
+        <h1 className="text-3xl font-bold text-red-600 mb-4">{props.header}</h1>
+        <p className="text-gray-600 mb-4">{props.code}</p>
+        <p className="text-gray-500">{props.info}</p>
+      </div>
   )
 }
 SpacielPage.propTypes = {
-    header: PropTypes.string,
-    code: PropTypes.string,
-    info: PropTypes.string
+  header: PropTypes.string,
+  code: PropTypes.string,
+  info: PropTypes.string
 }
 export default SpacielPage
