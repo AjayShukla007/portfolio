7b72670a36f7171fef2c7b3ea62f4afe1758dae2
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-02 20:05:14 +0530

    Working on Extra pages

--- src/components/extraPages/ExtraPage.jsx
+++ src/components/extraPages/ExtraPage.jsx
@@ -1,16 +1,28 @@
 import PropTypes from 'prop-types';
 
 const SpacielPage = (props) => {
+  const refreshPage = () => {
+    if (props.goTo === "Refresh") {
+      window.location.reload();
+      if (window.location.pathname === "/error") {
+        window.location.pathname = "/"
+      }
+    } else {
+      window.location.pathname = "/"
+    }
+    // console.log(window.location); //this is for debugging purpos
+  }
   return (
     <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-lg">
-        <h1>:)</h1>
-        <h1 className="text-3xl font-bold text-red-600 mb-4">{props.header}</h1>
-        <p className="text-gray-600 mb-4">{props.code}</p>
-        <p className="text-gray-500">{props.info}</p>
-      </div>
+      <h1>:(</h1><button onClick={refreshPage}>{props.goTo}</button>
+      <h1 className="text-3xl font-bold text-red-600 mb-4">{props.header}</h1>
+      <p className="text-gray-600 mb-4">{props.code}</p>
+      <p className="text-gray-500">{props.info}</p>
+    </div>
   )
 }
 SpacielPage.propTypes = {
+  goTo: PropTypes.string,
   header: PropTypes.string,
   code: PropTypes.string,
   info: PropTypes.string
