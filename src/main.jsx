1b3d8bc7b936609a95e15cfb28edf0faf9107cc4
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-02 20:06:23 +0530

    Removed Error boundary from main component

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router } from "react-router-dom";
-import ErrorBoundary from './components/Interactive/ErrorBoundary.jsx';
 import './index.css'
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <App />
     </Router>
   </React.StrictMode>
 )
