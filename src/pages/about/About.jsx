9d8fb4d41be38e3905da6dc047522ec2dbab7877
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-03 19:31:13 +0530

    Cleaning and removing unused code

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef } from 'react';
 import Intro from "./Intro.jsx";
 import Skills from "./Skills.jsx";
 import Edu from "./Education.jsx";
@@ -10,10 +10,8 @@ import {
   motion,
   useScroll,
   useTransform,
-  useVelocity,
-  useMotionValueEvent
+  useVelocity
 } from "framer-motion";
-import useElementScrollVelocity from "../../hook/ScrollVelocity.jsx";
 
 import IntroPreload, {SkillsPreLoad} from '../../components/PreLoaders/About.jsx';
 import {QuotesAbout, QuotesEdu, QuotesCert} from '../../components/Quotes/Quotes2.jsx';
@@ -34,67 +32,26 @@ const fetcher = async () => {
 };
 function About() {
   
-  /* const tempFunc = async ()=>{
-    try{
-      const data = await axios.get("https://dev.to/dev-ajayshukla/how-to-create-a-ripple-effect-with-tailwind-elements-react-2338");
-      console.log(data);
-    }catch(err){
-      console.log(err);
-    }
-  } */
-  // tempFunc()
   const { data, isLoading, error } = useQuery("aboutData", fetcher);
   const aboutRef = useRef(null);
   
-  const pink = saturation => `hsl(327, ${saturation}%, 50%)`;
-  const blur = blur => `blur(${blur || 0}px)`;
   const opac = opacity => `${opacity}`
   const { scrollY } = useScroll({
     target: aboutRef
   });
   
   const scrollVelocity = useVelocity(scrollY);
-  // const yVelocity = useElementScrollVelocity(mainRef);
-
-/*
-  
-  
-  // const { scrollY } = useScroll();
-  const scrollYMotionValue = useMotionValue(0);
-  scrollYMotionValue.set(scrollY);
-  const scrollVelocity = useVelocity(scrollYMotionValue);
-
-  
-  // const smoothVelocity = useSpring(scrollVelocity, {
-  //   damping: 50,
-  //   stiffness: 400
-  // });
-  // const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-  //   clamp: false
-  // });
-
- */ 
-  const scale = useTransform(scrollVelocity, [-3000, 0, 3000], [2, 1, 2], {
-    clamp: false
-  });
-  const backgroundColor = useTransform(scrollVelocity,
-    [-2000, 0, 2000],
-    [pink(100), pink(0), pink(100)]
-  );
-  let filter = useTransform(
-    scrollVelocity,
-    [-2000, 0, 2000],
-    [blur(50), blur(0), blur(50)]
-  );
+ 
   let opacity = useTransform(
     scrollVelocity,
     [-2000, 0, 2000],
     [opac(0.5), opac(1), opac(0.5)]
   );
-  
-  useMotionValueEvent(scrollY, "change", latest => {
-    // console.log(scale);
-  });
+
+
+  if (error && !isLoading) {
+    throw new Error(error)
+  }
   return (
     <motion.div className="aboutContainer" 
     ref={aboutRef}
