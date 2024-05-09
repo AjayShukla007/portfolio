3e6798d60af818080bd694931629f06077827e69
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-09 09:33:24 +0530

    Optimizing code for production

--- src/components/PreLoaders/Blogs.jsx
+++ src/components/PreLoaders/Blogs.jsx
@@ -1,5 +1,4 @@
-import { useState, useRef } from "react";
-import { motion, useSpring } from "framer-motion";
+import { motion } from "framer-motion";
 
 function BlogsPreload() {
   return (
@@ -22,173 +21,173 @@ function BlogsPreload() {
           scrollbarWidth: "none"
         }}
       >
-      <motion.div
-        style={{
-          background: "#808080",
-          borderRadius: "20px",
-          width: "100%",
-          height: '2em',          
-          padding: "5%",
-          scrollbarWidth: "none"
-        }}
-        animate={{
-          opacity: [0.7, 1, 0.7]
-        }}
-        transition={{
-          ease: "linear",
-          duration: 1.5,
-          repeat: Infinity
-        }}
-      >
-      
-      </motion.div>
-      <motion.div
-        style={{
-          background: "#808080",
-          width: "100%",
-          height: '6em',
-          borderRadius: '5px',
-          padding: '0.5em',
-          fontSize: '1.1em',
-          scrollbarWidth: "none"
-        }}
-        animate={{
-          opacity: [0.7, 1, 0.7]
-        }}
-        transition={{
-          ease: "linear",
-          duration: 2.5,
-          repeat: Infinity
-        }}
-      >
-      
-      </motion.div>
-      <div style={{
-        display:'flex',
-        flexDirection:'row',
-        gap:'1em'
-      }}>
         <motion.div
-        style={{
-          background: "#808080",
-          width: "10%",
-          height: '1.2em',
-          borderRadius: '15px',
-          padding: '0.5em',
-          fontSize: '1.1em',
-          scrollbarWidth: "none"
-        }}
-        animate={{
-          opacity: [0.7, 1, 0.7]
-        }}
-        transition={{
-          ease: "linear",
-          duration: 2.5,
-          repeat: Infinity
-        }}
-      >
-      </motion.div>
+          style={{
+            background: "#808080",
+            borderRadius: "20px",
+            width: "100%",
+            height: '2em',
+            padding: "5%",
+            scrollbarWidth: "none"
+          }}
+          animate={{
+            opacity: [0.7, 1, 0.7]
+          }}
+          transition={{
+            ease: "linear",
+            duration: 1.5,
+            repeat: Infinity
+          }}
+        >
+
+        </motion.div>
         <motion.div
-        style={{
-          background: "#808080",
-          width: "10%",
-          height: '1.2em',
-          borderRadius: '15px',
-          padding: '0.5em',
-          fontSize: '1.1em',
-          scrollbarWidth: "none"
-        }}
-        animate={{
-          opacity: [0.7, 1, 0.7]
-        }}
-        transition={{
-          ease: "linear",
-          duration: 2.5,
-          repeat: Infinity
-        }}
-      >
-      </motion.div>
-        <motion.div
-        style={{
-          background: "#808080",
-          width: "10%",
-          height: '1.2em',
-          borderRadius: '15px',
-          padding: '0.5em',
-          fontSize: '1.1em',
-          scrollbarWidth: "none"
-        }}
-        animate={{
-          opacity: [0.7, 1, 0.7]
-        }}
-        transition={{
-          ease: "linear",
-          duration: 2.5,
-          repeat: Infinity
-        }}
-      >
-      </motion.div>
-        <motion.div
-        style={{
-          background: "#808080",
-          width: "10%",
-          height: '1.2em',
-          borderRadius: '15px',
-          padding: '0.5em',
-          fontSize: '1.1em',
-          scrollbarWidth: "none"
-        }}
-        animate={{
-          opacity: [0.7, 1, 0.7]
-        }}
-        transition={{
-          ease: "linear",
-          duration: 2.5,
-          repeat: Infinity
-        }}
-      >
-      </motion.div>
-        <motion.div
-        style={{
-          background: "#808080",
-          width: "10%",
-          height: '1.2em',
-          borderRadius: '15px',
-          padding: '0.5em',
-          fontSize: '1.1em',
-          scrollbarWidth: "none"
-        }}
-        animate={{
-          opacity: [0.7, 1, 0.7]
-        }}
-        transition={{
-          ease: "linear",
-          duration: 2.5,
-          repeat: Infinity
-        }}
-      >
-      </motion.div>
-        <motion.div
-        style={{
-          background: "#808080",
-          width: "10%",
-          height: '1.2em',
-          borderRadius: '15px',
-          padding: '0.5em',
-          fontSize: '1.1em',
-          scrollbarWidth: "none"
-        }}
-        animate={{
-          opacity: [0.7, 1, 0.7]
-        }}
-        transition={{
-          ease: "linear",
-          duration: 2.5,
-          repeat: Infinity
-        }}
-      >
-      </motion.div>
-      </div>
+          style={{
+            background: "#808080",
+            width: "100%",
+            height: '6em',
+            borderRadius: '5px',
+            padding: '0.5em',
+            fontSize: '1.1em',
+            scrollbarWidth: "none"
+          }}
+          animate={{
+            opacity: [0.7, 1, 0.7]
+          }}
+          transition={{
+            ease: "linear",
+            duration: 2.5,
+            repeat: Infinity
+          }}
+        >
+
+        </motion.div>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '1em'
+        }}>
+          <motion.div
+            style={{
+              background: "#808080",
+              width: "10%",
+              height: '1.2em',
+              borderRadius: '15px',
+              padding: '0.5em',
+              fontSize: '1.1em',
+              scrollbarWidth: "none"
+            }}
+            animate={{
+              opacity: [0.7, 1, 0.7]
+            }}
+            transition={{
+              ease: "linear",
+              duration: 2.5,
+              repeat: Infinity
+            }}
+          >
+          </motion.div>
+          <motion.div
+            style={{
+              background: "#808080",
+              width: "10%",
+              height: '1.2em',
+              borderRadius: '15px',
+              padding: '0.5em',
+              fontSize: '1.1em',
+              scrollbarWidth: "none"
+            }}
+            animate={{
+              opacity: [0.7, 1, 0.7]
+            }}
+            transition={{
+              ease: "linear",
+              duration: 2.5,
+              repeat: Infinity
+            }}
+          >
+          </motion.div>
+          <motion.div
+            style={{
+              background: "#808080",
+              width: "10%",
+              height: '1.2em',
+              borderRadius: '15px',
+              padding: '0.5em',
+              fontSize: '1.1em',
+              scrollbarWidth: "none"
+            }}
+            animate={{
+              opacity: [0.7, 1, 0.7]
+            }}
+            transition={{
+              ease: "linear",
+              duration: 2.5,
+              repeat: Infinity
+            }}
+          >
+          </motion.div>
+          <motion.div
+            style={{
+              background: "#808080",
+              width: "10%",
+              height: '1.2em',
+              borderRadius: '15px',
+              padding: '0.5em',
+              fontSize: '1.1em',
+              scrollbarWidth: "none"
+            }}
+            animate={{
+              opacity: [0.7, 1, 0.7]
+            }}
+            transition={{
+              ease: "linear",
+              duration: 2.5,
+              repeat: Infinity
+            }}
+          >
+          </motion.div>
+          <motion.div
+            style={{
+              background: "#808080",
+              width: "10%",
+              height: '1.2em',
+              borderRadius: '15px',
+              padding: '0.5em',
+              fontSize: '1.1em',
+              scrollbarWidth: "none"
+            }}
+            animate={{
+              opacity: [0.7, 1, 0.7]
+            }}
+            transition={{
+              ease: "linear",
+              duration: 2.5,
+              repeat: Infinity
+            }}
+          >
+          </motion.div>
+          <motion.div
+            style={{
+              background: "#808080",
+              width: "10%",
+              height: '1.2em',
+              borderRadius: '15px',
+              padding: '0.5em',
+              fontSize: '1.1em',
+              scrollbarWidth: "none"
+            }}
+            animate={{
+              opacity: [0.7, 1, 0.7]
+            }}
+            transition={{
+              ease: "linear",
+              duration: 2.5,
+              repeat: Infinity
+            }}
+          >
+          </motion.div>
+        </div>
       </motion.div>
     </>
   );
