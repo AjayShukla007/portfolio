44efa1313bd7ba435fa93f1918be0dc82932b93e
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-09 09:32:23 +0530

    Optimized preloaders

--- src/components/PreLoaders/ProjectsPreload.jsx
+++ src/components/PreLoaders/ProjectsPreload.jsx
@@ -1,5 +1,5 @@
-import { useState, useRef } from "react";
-import { motion, useSpring, useInView } from "framer-motion";
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import { TERipple } from "tw-elements-react";
 
 function ProjectsPreload() {
