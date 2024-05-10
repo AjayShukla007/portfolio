34cadba98af5fad94a05be0ef0dbd89169e8f3f8
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-10 21:29:08 +0530

    Optimizing code for production

--- src/components/Nav/NavItems.jsx
+++ src/components/Nav/NavItems.jsx
@@ -1,22 +1,11 @@
-import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 // import { useDragControls } from "framer-motion";
 import { TERipple } from "tw-elements-react";
-import { useLocation, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import PropTypes from 'prop-types';
+
 import { useTitleData } from '../context/csContext.jsx'
 import "./styles/navItems.css";
-const variants = {
-  hover: {
-    scale: 1.1,
-    transition: {
-      duration: 0.5,
-      yoyo: Infinity
-    }
-  },
-  tap: {
-    scale: 0.9
-  }
-};
 
 function NavItems(props) {
   
@@ -50,70 +39,9 @@ function NavItems(props) {
     </div>
   );
 }
-
+NavItems.propTypes = {
+  link: PropTypes.string,
+  icon: PropTypes.string,
+  tooltip: PropTypes.string,
+}
 export default NavItems;
-// drag
-//             dragConstraints={{
-//               top: 0,
-//               left: 0,
-//               right: 0,
-//               bottom: 0
-//             }}
-/* ₹ūñŪŚĀḌḌṆṁṇ,ṁḷ̥
-  const dragControls = useDragControls();
-
-  // Use animations for div one and div two
-  const divOneAnimation = useAnimation();
-  const divTwoAnimation = useAnimation();
-
-  // Function to update animations based on div three's drag position
-  const updateAnimations = (x,y) => {
-    // Calculate the drag length
-    const dragLengthX = x;
-    const dragLengthY = y;
-  // console.log("x: "+x+" "+"y: "+y);
-    // Update animations for div two and div one
-    divTwoAnimation.start({
-      x: dragLengthX / 2,
-      y: dragLengthY / 2
-    });
-    divOneAnimation.start({
-      x: dragLengthX / 3,
-      y: dragLengthY / 3,
-    });
-  };
-
-  return (
-    <>
-      <motion.div className="mainContainer" animate={divOneAnimation}>
-        <motion.div className="itemNavItems" animate={divTwoAnimation}>
-          <motion.div
-            className="navIcon"
-            drag
-            dragConstraints={{
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0
-            }}
-            dragControls={dragControls}
-            onDrag={(e, info) => {
-              // Update animations on drag
-              // updateAnimations(info.point.x, info.point.y);
-              updateAnimations(info.delta.x, info.delta.y);
-              //console.log(info);
-            }}
-            onDragEnd={() => {
-          // Reset animations when drag is released
-          divTwoAnimation.start({ x: 0,y:0 });
-          divOneAnimation.start({ x: 0,y:0 });
-          // You can also reset div three's position if needed
-          // dragControls.start({ x: 0 });
-        }}
-          >
-            {props.icon}
-          </motion.div>
-        </motion.div>
-      </motion.div>
-    </>
-  );*/
