883ca37c7a4cd21057eedd1ecf80db9972b93044
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-10 21:29:16 +0530

    Optimizing code for production

--- src/components/Interactive/Time.jsx
+++ src/components/Interactive/Time.jsx
@@ -5,17 +5,20 @@ function Time() {
     const [time, setTime] = useState(new Date().toLocaleString().split(','))
 
     useEffect(() => {
-
         const updateTime = setInterval(() => {
             setTime(new Date().toLocaleString().split(','))
         }, 1000);
+
+        return () => {
+            clearInterval(updateTime);
+        }
 
     }, [time])
 
 
     return (
         <div className="time">
-            {time.map((data,i) => (
+            {time.map((data, i) => (
                 <div key={i}>{data}</div>
             ))}
         </div>
