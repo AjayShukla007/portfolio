3a6ab51f252ac346cfa0b603d634a8f7857198fb
AjayShukla007 <shuklaajay0838@gmail.com>
2024-05-15 10:10:20 +0530

    Improved code

--- src/pages/utility/Error.jsx
+++ src/pages/utility/Error.jsx
@@ -1,18 +1,21 @@
-import SpacielPage from '../../components/extraPages/ExtraPage'
-import PropTypes from 'prop-types';
+import SpacielPage from "../../components/extraPages/ExtraPage";
+import PropTypes from "prop-types";
 
 const Error = (props) => {
   return (
     <div>
-        <SpacielPage
+      <SpacielPage
         goTo={"Refresh"}
         header="Error"
-        code={props.errorCode || 'Some Unknown Error Occured'}
-        info={"This may not mean anything. \n \nI'm probably working on something that has blown up"}/>
+        code={props.errorCode || "Some Unknown Error Occured"}
+        info={
+          "This may not mean anything. \n \nI'm probably working on something that has blown up"
+        }
+      />
     </div>
-  )
-}
+  );
+};
 Error.propTypes = {
   errorCode: PropTypes.string,
-}
-export default Error
+};
+export default Error;
